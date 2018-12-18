b97dfeeadc26d0660d239fa4d7cf3980c93b2110
Vaishnavi K <45496791+vaichu22@users.noreply.github.com>
2018-12-19 00:51:44 +0530

    Added source file-Modules

---
+++ mains.py
@@ -0,0 +1,4 @@
+
+print("Inside main package")
+def func():
+    print(" FUNC() is in mains.py module within main package")
---
+++ one.py
@@ -0,0 +1,6 @@
+from Main_Package import mains
+from Main_Package.Sub_Package.subs import func2
+
+mains.func()
+func2()
+print(mains.list1)
