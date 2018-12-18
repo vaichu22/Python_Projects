f7940ab7ab607b5cda3d6323dec25ab7aa98d247
Vaishnavi K <45496791+vaichu22@users.noreply.github.com>
2018-12-19 00:49:27 +0530

    Added source file-Packages

---
+++ Main_Package/Sub_Package/subs.py
@@ -0,0 +1,9 @@
+
+
+print("Within Sub Package")
+
+def func2():
+    print("FUNC2() is in subs module within Sub Package")
+    
+name="vaishu"
+print(name)
---
+++ Main_Package/mains.py
@@ -0,0 +1,10 @@
+
+print("Inside main package")
+def func():
+    print(" FUNC() is in mains.py module within main package")
+tup=(10,20,30)
+print(tup)
+list1=['asdf',34,"qwerty"]
+    
+if __name__ == "__main__":
+    func()
